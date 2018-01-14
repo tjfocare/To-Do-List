5d8d90a34a9fb9dc5da486f93e0999b7cb6c3e51
Tom Focareta <meecrobjr@hotmail.com>
2018-01-14 21:39:56 +1030

    Initial commit, display cells

---
+++ To-Do List/ToDoViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  To-Do List
+//
+//  Created by Thomas Focareta on 14/1/18.
+//  Copyright © 2018 Thomas Focareta. All rights reserved.
+//
+
+import UIKit
+
+class ToDoViewController: UITableViewController {
+
+    let itemArray = ["Find dad", "Buy cheese", "Eat leftovers"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+}
+
